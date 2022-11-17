0f3bd6b008fba0242afa4db419f04e490897dcd4
tommygents <tmaranges+gh@gmail.com>
2022-11-17 17:58:04 -0500

    added needed libraries

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let ball, floor;
+
 function setup() {
   createCanvas(400, 400);
+  world.gravity.y = 10;
+
+	ball = new Sprite();
+	ball.diameter = 50;
+	ball.y = 30;
+
+	floor = new Sprite();
+	floor.collider = 'static';
+	floor.y = 190;
+	floor.w = 400;
+	floor.h = 5;
 }
 
 function draw() {
-  background(220);
+  clear();
+
 }
